600c523b55e4279b55d84af867807ca3e24a4505
devMeesh0 <44327689+devMeesh0@users.noreply.github.com>
2023-04-03 14:57:55 -0400

    updated comments and todo lines

--- Message.java
+++ Message.java
@@ -4,6 +4,14 @@ public class Message {
     private User reciever;
     private String timestamp;
 
-
+    //TODO: need constructor, gettters and setters for all above variables, implement specific methods marked below
     
+    
+
+    //TODO: method that creates conversation file if need be, then adds message to conversation text file 
+    public boolean updateConversation(Message message) {
+        
+        return false;
+    }
+
 }
--- User.java
+++ User.java
@@ -6,22 +6,28 @@ public class User {
     public int phoneNum;
     public String address;
 
+    //TODO: complete constructor, add getters and setters for fields, and complete methods marked below
+
     public User() {
 
     }
 
+    //TODO: using scanner, ask the user to input their info again (name, phone num, address)
     public void modifyUser() {
 
     }
 
+    //TODO: take username, password, and info out of all text files. set all values in this instance to null or zero
     public void deleteUser() {
 
     }
 
+    //TODO: add entry in blocked.txt detailing who is blocking who
     public void blockUser(User blocked) {
         
     }
 
+    //TODO: add entry in invisible.txt detailing who is being made invisible to who
     public void makeInvisible(User ghost) {
         
     }

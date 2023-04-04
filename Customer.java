c0edf4b5cc19cc973903800ddebe004424760d03
Alexkt520 <sun1116@purdue.edu>
2023-04-04 15:04:30 -0400

    Some changes for User.java and Customer.java

--- Customer.java
+++ Customer.java
@@ -1,16 +1,47 @@
 import java.util.*;
+import java.io.*;
+/**
+ * CS180 PJ 4
+ *
+ * Customer class extends User
+ *
+ * @author Jiatong Sun
+ * @version 4/4/2023
+ */
 
 public class Customer extends User {
     public static ArrayList<Seller> sellers;
-    
     public Customer() {
     
     }
-
-    //TODO: method that loads sellers arraylist with list of sellers from user.txt
-    public static ArrayList<Seller> viewListOfSellers() {
-        return sellers;
+    
+    public Customer(String username, String password, String name, int numMessages, int phoneNum, String address, ArrayList<Seller> sellers) {
+        super(username, password, name, numMessages,phoneNum,address);
+        this.sellers = sellers;
     }
     
-    
+    public static ArrayList<Seller> viewListOfSellers() {
+        //TODO: method that loads sellers arraylist with list of sellers from user.txt
+        File f = new File("user.txt");
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(f);
+            br = new BufferedReader(fr);
+            ArrayList<String> sellerList = new ArrayList<>();
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sellerList.add(line);
+            }
+            return sellerList; // need to change
+        } catch (FileNotFoundException e) {
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    public void searchForCustomers(String fileName ,String information) {
+        //have some questions
+    }
 }
+
--- User.java
+++ User.java
@@ -1,36 +1,107 @@
+import java.util.Scanner;
+
 public class User {
+    
+    
     public String username;
     public String password;
-    public String Name;
+    public String name;
     public int numMessages;
     public int phoneNum;
     public String address;
-
-    //TODO: complete constructor, add getters and setters for fields, and complete methods marked below
-
     public User() {
-
+    
     }
-
+    public User(String username, String password, String name, int numMessages, int phoneNum, String address) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.numMessages = numMessages;
+        this.phoneNum = phoneNum;
+        this.address = address;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        name = name;
+    }
+    
+    public int getNumMessages() {
+        return numMessages;
+    }
+    
+    public void setNumMessages(int numMessages) {
+        this.numMessages = numMessages;
+    }
+    
+    public int getPhoneNum() {
+        return phoneNum;
+    }
+    
+    public void setPhoneNum(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    //TODO: complete constructor, add getters and setters for fields, and complete methods marked below
+    
+    
+    
     //TODO: using scanner, ask the user to input their info again (name, phone num, address)
     public void modifyUser() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String newName = scanner.nextLine();
+        System.out.println("Please enter your phone number: ");
+        int newPhoneNum = scanner.nextInt();
+        System.out.println("Please enter your address: ");
+        String newAddress = scanner.nextLine();
+        this.name = newName;
+        this.phoneNum = newPhoneNum;
+        this.address = newAddress;
+        System.out.println("User information updated.");
+        scanner.close();
     }
-
+    
     //TODO: take username, password, and info out of all text files. set all values in this instance to null or zero
     public void deleteUser() {
-
+    
     }
-
+    
     //TODO: add entry in blocked.txt detailing who is blocking who
     public void blockUser(User blocked) {
         
     }
-
+    
     //TODO: add entry in invisible.txt detailing who is being made invisible to who
     public void makeInvisible(User ghost) {
         
     }
-
+    
     
 }

e6831221cca46b4bbb87a431989dec2c413d4a7f
Alexkt520 <sun1116@purdue.edu>
2023-04-05 21:51:09 -0400

    Finish version1 Customer

--- Customer.java
+++ Customer.java
@@ -21,28 +21,86 @@ public class Customer extends User {
         this.sellers = sellers;
     }
     
+    public ArrayList<Seller> getSellers() {
+        return sellers;
+    }
+    
+    public void setSellers(ArrayList<Seller> sellers) {
+        this.sellers = sellers;
+    }
+    
     public static ArrayList<Seller> viewListOfSellers() {
         //TODO: method that loads sellers arraylist with list of sellers from user.txt
+        ArrayList<Seller> sellers = new ArrayList<>();
         File f = new File("user.txt");
         FileReader fr = null;
         BufferedReader br = null;
         try {
             fr = new FileReader(f);
             br = new BufferedReader(fr);
-            ArrayList<String> sellerList = new ArrayList<>();
             String line = "";
             while ((line = br.readLine()) != null) {
-                sellerList.add(line);
+                String[] parts = line.split(",");
+                Seller seller = new Seller(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
+                sellers.add(seller);
             }
-            return sellerList; // need to change
+            return sellers; // need to change
         } catch (FileNotFoundException e) {
             return null;
         } catch (Exception e) {
             return null;
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+    
     public void searchForCustomers(String fileName ,String information) {
-        //have some questions
+        try {
+            Scanner scanner = new Scanner(System.in);
+            File f = new File(fileName);
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String input = scanner.nextLine();
+                String[] part = line.split(",");
+                if (part.length != 7) {
+                    continue;
+                }
+                String userType = part[0];
+                String userName = part[1];
+                String password = part[2];
+                String name = part[3];
+                int numMessages = Integer.parseInt(part[4]);
+                int phoneNum = Integer.parseInt(part[5]);
+                String address = part[6];
+                if (userName.contains(information) ||
+                        name.contains(information) ||
+                        phoneNum == Integer.parseInt(information) ||
+                        address.contains(information)) {
+                    System.out.printf("User Type: %s\nUser Name: %s\nPassword: %s" +
+                                    "\nName: %s\nNumber of Message: \n" +
+                                    "Phone Number: %s\nAddress: %s",
+                            userType, userName, password, name, numMessages, phoneNum, address);
+                    System.out.println();
+                }
+                
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
 

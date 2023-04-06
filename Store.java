a6b4de19a0b4c6064dd0686013a9c4254f372762
nrusk <56550361+nrusk@users.noreply.github.com>
2023-04-05 20:31:27 -0400

    Update Store.java

--- Store.java
+++ Store.java
@@ -1,11 +1,47 @@
+/**
+ * CS180 PJ 4
+ *
+ * Store class
+ *
+ * @author Nathar Rusk
+ * @version 4/5/2023
+ */
 public class Store {
     public String name;
     public String storeType;
     public String address;
 
     //TODO: create constructor, getters and setters
-    
-    
+    public Store(String name, String storeType, String address) {
+        this.name = name;
+        this.storeType = storeType;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStoreType() {
+        return storeType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStoreType(String storeType) {
+        this.storeType = storeType;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     //TODO: method that takes store and adds it to stores.txt
     public void publish() {
 
